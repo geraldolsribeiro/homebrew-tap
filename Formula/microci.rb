e91aa0e878672fb3e7a8eb47d410ce7786c12cc8
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2025-09-07 21:01:40 +0400

    Update formula

--- Formula/microci.rb
+++ Formula/microci.rb
@@ -1,17 +1,17 @@
 class Microci < Formula
   desc "Minimalist continuous integration tool"
   homepage "https://microci.dev"
-  url "https://github.com/geraldolsribeiro/microCI/archive/refs/tags/v0.40.1.tar.gz"
-  #sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  url "https://github.com/geraldolsribeiro/microCI/archive/refs/tags/v0.40.2.tar.gz"
+  sha256 "d7f25c5dc64cde6948f8bd6d639def2b2b138a55d3a5e9d09ee4fb1838dfba4e"
   license "MIT"
   head "https://github.com/geraldolsribeiro/microci.git", branch: "master"
 
-  depends_on "inja"
+  depends_on "gettext"
   depends_on "spdlog"
   depends_on "yaml-cpp"
 
   def install
-    system "make", "-C", "src", "CXXFLAGS=-std=c++17 -DFMT_HEADER_ONLY -DSPDLOG_FMT_EXTERNAL=1 -I/opt/homebrew/Cellar/inja/3.4.0/include/inja -I../include/"
+    system "make", "-C", "src", "CXXFLAGS=-std=c++17 -DFMT_HEADER_ONLY -DSPDLOG_FMT_EXTERNAL=1 -I../include/"
     bin.mkpath
     bin.install "bin/microCI"
   end
@@ -26,6 +26,6 @@ class Microci < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    system "bin/microCI", "--version"
   end
 end
